6c495f28bb82f87efbae50f074e65874b5105ded
nachiket83 <78677493+nachiket83@users.noreply.github.com>
2021-07-12 11:19:50 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+def countwords():
+    filename = input("Enter the file name:")
+    f = open(filename,'w')
+    for line in f:
+        word = line.split()
+        num = num+len(words)
+    print(num)
+countwords()
+
+
